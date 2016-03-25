c11f62a4cf4ad48c930525c9aa33ff4f4f90958b
Sarn Wattanasri <mr.paxton@gmail.com>
2016-03-24 18:36:21 -0700

    Initial Commit

---
+++ TinderCodepath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderCodepath
+//
+//  Created by Sarn Wattanasri on 3/24/16.
+//  Copyright © 2016 Sarn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
